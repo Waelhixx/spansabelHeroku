70de1269f9b3d1daf4f88841593cb573a779092f
sabelspan <sabelspan>
2022-05-31 01:51:29 +0800

    Initail Deploy

---
+++ Final-Project-Jaspe/gjprj/pmi/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.contrib.admin import DateFieldListFilter
+from .models import Product
+
+class ProductAdmin(admin.ModelAdmin):
+  fields = ('code','name','price','description','date_entry','photo')
+  list_filter = (
+    ('date_entry', DateFieldListFilter),
+  )
+  list_display = ('code','name','price','description','date_entry')
+
+admin.site.register(Product,ProductAdmin)
+
